c92e67cd12c5ae801b410d26af51414e4d96f359
brendan <101201@glr.nl>
2025-03-17 15:27:13 +0100

    navigatie af

--- bioSyn/src/App.jsx
+++ bioSyn/src/App.jsx
@@ -8,27 +8,27 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="navbar">
+        <div class="hamburger" onClick={() => {
+          const menu = document.getElementById('menu');
+          menu.classList.toggle('menu-open');
+        }}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <h1 class="header">BioSyn</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav id="menu">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="pages/About.html">About</a></li>
+          <li><a href="pages/Profile.html">Profile</a></li>
+        </ul>
+      </nav>
+
     </>
+
   )
 }
 
